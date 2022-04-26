e04b18ade646eb90f67fbc9bbe0fd2765245eb0f
avandefeniks <angel.vandefeniks@gmail.com>
2022-04-26 16:11:28 -0700

    working on adding cities list to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,44 @@
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector(".form-input");
-var cityList = document.querySelector("#city-list")
+var cityListEl = document.querySelector("#city-list");
+var cityButtonsEl = document.querySelectorAll(".btn-list");
 
 function formHandler(event) {
-// disable form default
-event.preventDefault();
+    // disable form default
+    event.preventDefault();
 
-// get city from input field
-var city = cityInputEl.value;
-console.log(city);
+    // get city from input field
+    var city = cityInputEl.value;
+    console.log(city);
 
-var listItemEl = document.createElement("li");
-listItemEl.innerHTML = "<input type='button' class='btn-list' value=" + city + ">"
-console.log(listItemEl.innerHTML);
+    // add cities to list
+    var listItemEl = document.createElement("li");
+    listItemEl.innerHTML = "<input type='button' class='btn-list' value=" + "'" + city + "'" + ">"
+    console.log(listItemEl.innerHTML);
+    cityListEl.appendChild(listItemEl);
 
+    cityInputEl.value = "";
+
+    // saveCityList(city);
 
 }
 
+function saveCityList() {
+    // console.log(string1 + " is a nice city")
+    console.log(cityButtonsEl)
+
+    var citiesArr = [];
+
+    // loop through cities list
+    // for (var i = 0; i < cityButtonsEl.length; i++) {
+    //     // add cities to array
+    //     citiesArr.push(cityButtonsEl);
+    //     console.log(citiesArr);
+    // }
+
+    // add array to local storage
+    // localStorage.setItem("cities", JSON.stringify(citiesArr));
+
+}
+saveCityList();
 searchFormEl.addEventListener("submit", formHandler);;
